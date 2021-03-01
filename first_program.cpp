6a209f06306b68779ac0fda0fc1c510acad57582
KaemonIsland <k-manskrazy@hotmail.com>
2021-02-28 19:04:37 -0700

    Add hello world program

---
+++ first_program.cpp
@@ -0,0 +1,14 @@
+// Include a library before compilation
+// Do not need semicolon at the end of this line
+#include <iostream>
+
+// Every C++ program requires "int main()"
+// It is the entry point of a c++ program
+// Without this, the program won't build!
+int main() // int means that we are returning an integer
+{
+  std::cout << "Hello World!";
+  // 0 states that the code has run correctly
+  // Any other number will return an error
+  return 0;
+}
